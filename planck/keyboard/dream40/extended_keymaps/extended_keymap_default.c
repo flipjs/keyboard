524e24d35ab204cc9320064f896aabb32d727c41
Felipe Apostol <flipjs.io@gmail.com>
2015-12-20 07:47:54 +0000

    create new branch called test and update keybindings to use tabfn and enterfn as number and punctuation layers

--- planck/keyboard/dream40/extended_keymaps/extended_keymap_default.c
+++ planck/keyboard/dream40/extended_keymaps/extended_keymap_default.c
@@ -7,15 +7,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = { /* Qwerty */
 {KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-{FUNC(5),  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_NO,   KC_ENT},
+{FUNC(5),  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_NO,   FUNC(8)},
 {KC_LSFT, KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,   KC_N,    KC_M,    KC_NO,   KC_COMM, KC_DOT,  KC_RSFT},
-{KC_LALT, KC_LGUI, KC_LCTL,  KC_SPC,   KC_NO,   KC_NO,  FUNC(6), KC_NO,   FUNC(8), KC_NO,   FUNC(7), FUNC(10)}
+{KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,   KC_NO,   KC_NO,  FUNC(6), KC_NO,   FUNC(9), KC_NO,   FUNC(7), FUNC(10)}
 },
 
 [1] = { /* Cursor */
 {KC_TRNS, KC_BSPC, KC_NO,   KC_UP,   KC_NO,   KC_HOME, KC_PGUP, KC_NO,   KC_UP,   KC_NO,   FUNC(15), FUNC(14)},
-{KC_TRNS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,    KC_TRNS},
-{KC_TRNS, KC_DEL,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_DEL},
+{KC_TRNS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,    KC_DEL},
+{KC_TRNS, KC_DEL,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_TRNS},
 {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_TRNS, KC_NO,   KC_TRNS,  KC_TRNS}
 },
 
@@ -23,14 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 {KC_TRNS, KC_SLSH, S(KC_1), S(KC_2),    S(KC_3),    S(KC_8),   S(KC_QUOT), S(KC_9),    S(KC_0),    S(KC_GRV),  KC_BSLS, KC_TRNS},
 {KC_TRNS, KC_MINS, S(KC_4), S(KC_5),    S(KC_6),    S(KC_EQL), KC_QUOT,    S(KC_LBRC), S(KC_RBRC), S(KC_SLSH), KC_NO,   KC_TRNS},
 {KC_TRNS, KC_EQL,  S(KC_7), S(KC_BSLS), S(KC_MINS), KC_GRV,    KC_LBRC,    KC_RBRC,    KC_NO,      S(KC_SCLN), KC_SCLN, KC_TRNS},
-{KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_NO,      KC_NO,     KC_TRNS,    KC_NO,      KC_TRNS,    KC_NO,      KC_TRNS, KC_TRNS}
+{KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,    KC_NO,      KC_NO,     KC_DOT,    KC_NO,      KC_TRNS,    KC_NO,      KC_TRNS, KC_TRNS}
 },
 
 [3] = { /* Number */
 {KC_TRNS, KC_SLSH, KC_1, KC_2,    KC_3,  S(KC_8),   S(KC_QUOT), S(KC_9),    S(KC_0),    S(KC_GRV),  KC_BSLS, KC_TRNS},
 {KC_TRNS, KC_MINS, KC_4, KC_5,    KC_6,  S(KC_EQL), KC_QUOT,    S(KC_LBRC), S(KC_RBRC), S(KC_SLSH), KC_NO,   KC_TRNS},
 {KC_TRNS, KC_EQL,  KC_7, KC_8,    KC_9,  KC_GRV,    KC_LBRC,    KC_RBRC,    KC_NO,      S(KC_SCLN), KC_SCLN, KC_TRNS},
-{KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_NO, KC_NO,     KC_TRNS,     KC_NO,      KC_TRNS,    KC_NO,      KC_TRNS, KC_TRNS}
+{KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_NO, KC_NO,     KC_DOT,     KC_NO,      KC_TRNS,    KC_NO,      KC_TRNS, KC_TRNS}
 },
 
 [4] = { /* Function */
@@ -54,10 +54,10 @@ const uint16_t PROGMEM fn_actions[] = {
     [3] = ACTION_LAYER_MOMENTARY(3),                                      // Number
     [4] = ACTION_LAYER_MOMENTARY(4),                                      // Function
     [5] = ACTION_LAYER_TAP_KEY(3, KC_TAB),                                // TabFn Number
-    [6] = ACTION_LAYER_TAP_KEY(2, KC_SPC),                                // SpaceFn Punctuation
+    [6] = ACTION_LAYER_TAP_KEY(1, KC_SPC),                                // SpaceFn Cursor
     [7] = ACTION_LAYER_TAP_KEY(4, KC_PAUS),                               // PauseFn Function
-    [8] = ACTION_LAYER_TAP_KEY(1, KC_SLSH),                               // SlashFn Cursor
-    [9] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_TAB),                          // Hold=Ctrl, Tap=Tab
+    [8] = ACTION_LAYER_TAP_KEY(2, KC_ENT),                               // EnterFn Punctuation
+    [9] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_SLSH),                          // Hold=Ctrl, Tap=Slash
     [10] = ACTION_MODS_TAP_KEY(MOD_LCTL|MOD_LSFT|MOD_LALT, KC_BSLS),      // Hold=Ctrl+Alt+Shift, Tap=Backslash
     [11] = ACTION_MODS_TAP_KEY(MOD_LCTL|MOD_LALT, KC_ESC),                // Hold=Ctrl+Alt, Tap=Esc
     [12] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_MUTE),                        // Hold=Ctrl, Tap=Mute
